513f1474b5732113d5010bdb5ab12ac11972b6bb
usama2333 <usama.wizz@gmail.com>
2024-10-16 15:04:25 +0500

    Theme provider

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -6,24 +6,26 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useTheme } from "@/components/theme-provider";
 // import { useTheme } from "@/components/theme-provider";
 
 // import "./ImageSlider.css";
 
 export default function ClientRoutePage() {
+    const theme = useTheme();
     const settings = {
         dots: true
     }
 //   const theme = useTheme();
   return (
     <>
-      {/* <h1
+      <h1
         style={{
-          color: theme.colors.secondary,
+          color: theme.colors.primary,
         }}
       >
         Server Route
-      </h1> */}
+      </h1>
       <div className="image-slider-container">
         <Slider {...settings}>
           <div>
